6f24c5abcff1f14332598098d95b9370303ef4e3
DevAbdelrahmanSayed <devabdelr2hman@gmail.com>
2024-01-04 23:30:21 +0300

    fix the ResetPasswordRequest

--- Modules/User/app/Http/Requests/ResetPasswordRequest.php
+++ Modules/User/app/Http/Requests/ResetPasswordRequest.php
@@ -2,7 +2,10 @@
 
 namespace Modules\User\app\Http\Requests;
 
+use App\Helpers\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\Rules\Password;
 
 class ResetPasswordRequest extends FormRequest
@@ -17,6 +20,14 @@ class ResetPasswordRequest extends FormRequest
             'password' => ['required', 'regex:/^[^<>]*$/', Password::min(8)->mixedCase()->numbers()->symbols()->uncompromised(), Password::defaults()],
 
         ];
+
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        if (request()->is('api/*')) {
+            $response = ApiResponse::sendResponse(422, 'Validation Errors', $validator->errors());
+            throw new HttpResponseException($response);
+        }
     }
 
     /**
