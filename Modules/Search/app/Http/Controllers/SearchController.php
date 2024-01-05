513f2f6dd5c47676e52db4414ad978b18af0c98d
DevAbdelrahmanSayed <devabdelr2hman@gmail.com>
2024-01-05 13:20:27 +0300

    add the searchResource IN search module

--- Modules/Search/app/Http/Controllers/SearchController.php
+++ Modules/Search/app/Http/Controllers/SearchController.php
@@ -9,21 +9,19 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Modules\Post\app\Models\Post;
+use Modules\Post\app\Resources\PostsResource;
+use Modules\Search\app\Resources\SearchResource;
 use Modules\User\app\Resources\LoginResource;
 
 class SearchController extends Controller
 {
     public function index(Request $request)
     {
-       $searchTerms = $request->input('text');
+        $searchTerms = $request->input('text');
 
-       $posts = Post::search($searchTerms)->get();
-       if ($posts) {
-           return ApiResponse::sendResponse(JsonResponse::HTTP_OK, 'Search results', $posts);
-       }
-
-        return ApiResponse::sendResponse(JsonResponse::HTTP_OK, 'No Search results try again');
+        $posts = Post::search($searchTerms)->get();
+        if ($posts) {
+            return ApiResponse::sendResponse(JsonResponse::HTTP_OK, 'Search results', SearchResource::collection($posts));
+        }
     }
-
-
 }
---
+++ Modules/Search/app/Resources/SearchResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Search\app\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SearchResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'category' => $this->category_id,
+            'owner_name' => $this->user->name_,
+            'pet_photo' => $this->pet_photo,
+            'pet_favorite' =>$this->isFavoritedByUser(auth()->id()),
+            'pet_type' => $this->pet_type,
+            'pet_name' => $this->pet_name,
+            'pet_gender' => $this->pet_gender,
+            'pet_age' => $this->pet_age,
+            'pet_breed' => $this->pet_breed,
+            'pet_desc' => $this->pet_desc,
+            'contact_number' =>$this->user->contact_number,
+            'country' => $this->user->country,
+            'address' => $this->user->address,
+            'created_at' => Carbon::parse($this->created_at)->toTurkey(),
+        ];
+    }
+}
